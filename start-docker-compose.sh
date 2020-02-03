8c08cd9ee627ecc699ed30ecc5c3698618bb85ad
planeta <info@planetacomputer>
2020-02-03 18:10:49 +0100

    permisos proxy

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -2,3 +2,5 @@
 docker-compose up -d
 docker exec -it client bash -c /usr/local/bin/client.sh
 docker exec -it proxy bash -c /usr/local/bin/proxy.sh
+docker exec -it proxy bash -c "chmod +x /usr/local/bin/proxy.sh"
+docker exec -it client bash -c "chmod +x /usr/local/bin/client.sh"
